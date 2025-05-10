b8514ea56b9c25c7a1683a07017a2d0e5c3afce6
Kzr-dev <salopard1337@gmail.com>
2025-05-10 21:25:41 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+const { Client } = require('discord.js-selfbot-v13');
+const axios = require('axios');
+
+const client = new Client({
+    checkUpdate: false
+});
+
+const token = "your token";
+const clanIDList = [
+    "1st",
+    "2nd, and more if you want"
+]; 
+
+
+client.on('ready', () => {
+
+    console.log(`${client.user.displayName} is ready`);
+
+    setInterval(() => {
+        const guildID = clanIDList[index % clanIDList.length];
+
+        axios.put('https://discord.com/api/v9/users/@me/clan', {
+            identity_guild_id: guildID,
+            identity_enabled: true
+        }, {
+            headers: {
+                Authorization: token,
+                'Content-Type': 'application/json'
+            }
+        }).then(res => {
+            console.log(`Guild changed to ${guildID}`);
+        }).catch(err => {
+            console.error(`Error:`, err.response?.data || err.message);
+        });
+    }, 30000); //30 sec but you can change (its milliseconds)
+    
+    
+});
+
+client.login(token);
